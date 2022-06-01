9fbec9e94d9d78e38db8a1be1261bf42c01c1ff8
aaakpinar <alperen.akpinar@nokia.com>
2022-06-01 13:59:13 +0200

    get update

--- cliplugin/show fabric/get-show-fabric.sh
+++ cliplugin/show fabric/get-show-fabric.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 wget -O /etc/opt/srlinux/cli/plugins/fabric.py  https://raw.githubusercontent.com/aaakpinar/srlinux/main/cliplugin/show%20fabric/fabric.py 
+wget -O ./get-show-fabric.sh https://raw.githubusercontent.com/aaakpinar/srlinux/main/cliplugin/show%20fabric/set-show-fabric.sh
 wget -O ./set-show-fabric.sh https://raw.githubusercontent.com/aaakpinar/srlinux/main/cliplugin/show%20fabric/set-show-fabric.sh
-
-chmod +x ./set-show-fabric.sh
+wget -O ./delete-show-fabric.sh https://raw.githubusercontent.com/aaakpinar/srlinux/main/cliplugin/show%20fabric/delete-show-fabric.sh
+chmod +x ./get-show-fabric.sh ./set-show-fabric.sh ./delete-show-fabric.sh
 ./set-show-fabric.sh
 sr_cli
