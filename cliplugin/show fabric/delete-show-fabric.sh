4263b444d9e0d7c3b0db66f85fa14ead1fd1d4fa
aaakpinar <alperen.akpinar@nokia.com>
2022-06-01 15:57:18 +0200

    Update delete-show-fabric.sh

--- cliplugin/show fabric/delete-show-fabric.sh
+++ cliplugin/show fabric/delete-show-fabric.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-rm -f /etc/opt/srlinux/cli/plugins/fabric.py && rm -f ./set-show-fabric.sh
+rm -f /etc/opt/srlinux/cli/plugins/fabric.py && echo fabric.py is removed!
+rm -f ./set-show-fabric.sh && echo set script is removed!
 
-read -p "Do you want to remove get script as well? (Yes/No) [No]" get_remove
+read -p "Do you want to remove the get script as well? (Yes/No) [No]" get_remove
 get_remove=${get_remove:-No}
 
 if [ $get_remove == "Yes" ]; then
-   rm -f ./get-show-fabric.sh || echo "Failed to remove get script! It may not be at $PATH."
+   SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+   rm -rf $SCRIPT_DIR  || echo "Kaboom! It's gone!"
+else
+   rm $0 && echo "Removing the delete script!"
 fi
-
-rm $0 && echo "Removing the delete script!"
