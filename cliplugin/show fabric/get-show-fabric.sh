ec8f978627d431be89ab555e70f6d978a089b31f
aaakpinar <alperen.akpinar@nokia.com>
2022-06-01 14:57:07 +0200

    Update get-show-fabric.sh

--- cliplugin/show fabric/get-show-fabric.sh
+++ cliplugin/show fabric/get-show-fabric.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget -O /etc/opt/srlinux/cli/plugins/fabric.py  https://raw.githubusercontent.com/aaakpinar/srlinux/main/cliplugin/show%20fabric/fabric.py 
-if ${PWD##*/} == "show-fabric"; then mkdir -p show-fabric; fi
+if [${PWD##*/} == "show-fabric"]; then mkdir -p show-fabric; fi
 wget -O ./show-fabric/get-show-fabric.sh https://raw.githubusercontent.com/aaakpinar/srlinux/main/cliplugin/show%20fabric/get-show-fabric.sh
 wget -O ./show-fabric/set-show-fabric.sh https://raw.githubusercontent.com/aaakpinar/srlinux/main/cliplugin/show%20fabric/set-show-fabric.sh
 wget -O ./show-fabric/delete-show-fabric.sh https://raw.githubusercontent.com/aaakpinar/srlinux/main/cliplugin/show%20fabric/delete-show-fabric.sh
