8c49962dcb7c2f4ea09c633314a7fa6f3d508f96
aaakpinar <alperen.akpinar@nokia.com>
2022-08-23 14:25:56 +0200

    Update and rename oper-group-bgp.py to oper-group-ebgp.py

--- event-handlers/oper-group-ebgp.py
+++ event-handlers/oper-group-ebgp.py
@@ -1,17 +1,19 @@
 import sys
 import json
 
-# count_up_uplinks returns the number of monitored uplinks that have oper-state=up
-def count_up_uplinks(paths):
+# count_ebgp_sessions_established returns the number of monitored eBGP sessions that are established  {established=up}
+def count_ebgp_sessions_established(paths):
     up_cnt = 0
     for path in paths:
-        if path.get("value", "down") == "up":
+        if path.get("value") == "established":
             up_cnt = up_cnt + 1
     return up_cnt
 
-# required_up_uplinks returns the value of the `required-up-uplinks` option
-def required_up_uplinks(options):
-    return int(options.get("required-up-uplinks", 1))
+
+# required_ebgp_sessions_established returns the value of the `required-ebgp-sessions-established` option
+def required_ebgp_sessions_established(options):
+    return int(options.get("required-ebgp-sessions-established", 1))
+
 
 # main entry function for event handler
 def event_handler_main(in_json_str):
@@ -20,16 +22,15 @@ def event_handler_main(in_json_str):
     paths = in_json["paths"]
     options = in_json["options"]
 
-    num_up_uplinks = count_up_uplinks(paths)
+    num_up_ebgp_sessions = count_ebgp_sessions_established(paths)
     downlinks_new_state = (
-        "down" if num_up_uplinks < required_up_uplinks(options) else "up"
+        "down" if num_up_ebgp_sessions < required_ebgp_sessions_established(options) else "up"
     )
 
-    # add `debug="true"` option to event-handler configuration to output parsed parameters
     if options.get("debug") == "true":
         print(
-            f"num of required up uplinks = {required_up_uplinks(options)}\n\
-detected num of up uplinks = {num_up_uplinks}\n\
+            f"\nnum of required eBGP sessions established = {required_ebgp_sessions_established(options)}\n\
+detected num of eBGP sessions established = {num_up_ebgp_sessions}\n\
 downlinks new state = {downlinks_new_state}"
         )
 
